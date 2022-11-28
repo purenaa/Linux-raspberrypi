bb90c1b4989a6953e2a32ab6279df155f182cafd
purenaa <jyn8313@gmail.com>
2022-11-28 01:17:40 -1000

    221128

---
+++ helloopencv.cpp
@@ -0,0 +1,11 @@
+#include <opencv2/highgui/highgui.hpp>
+
+using namespace cv;
+
+int main()
+{
+    Mat image(300, 400, CV_8UC1, Scalar(255));
+    imshow("Hello Wolrd", image);
+    waitKey(0);
+    return 0;
+}
